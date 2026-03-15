dae430e7587aaf1e8b0460d2ed4b1b72571a1dd1
Md Raja <mdraja.tech.03@gmail.com>
2026-03-15 13:05:29 +0530

    Update script.js

--- script.js
+++ script.js
@@ -119,8 +119,6 @@ const observer = new IntersectionObserver((entries)=>{
     entries.forEach((entry)=>{
         if(entry.isIntersecting){
             entry.target.classList.add("show-items");
-        }else{
-            entry.target.classList.remove("show-items");
         }
     });
 });
